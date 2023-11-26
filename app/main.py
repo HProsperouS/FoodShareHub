b452e374529e4f5215499d120ab4cc8e97761296
Liu JiaJun <jiajunliupersonal@gmail.com>
2023-11-26 12:42:57 +0800

    add bootstrap flash message

--- app/main.py
+++ app/main.py
@@ -2,6 +2,12 @@ from fastapi import FastAPI, Request
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
 from starlette.staticfiles import StaticFiles
+from fastapi.responses import (
+    FileResponse, 
+    ORJSONResponse
+)
+# import local libraries
+import utils.constants as C
 
 app = FastAPI()
 
@@ -16,12 +22,24 @@ async def home(request: Request):
     data = {
         "page": "Home page"
     }
-    return templates.TemplateResponse("page.html", {"request": request, "data": data})
+    flash_message_text = "This is a custom flash message!"
+    flash_message_class = "alert-success"  
+    # return templates.TemplateResponse("index.html", {"request": request, "data": data, "flash_message_text": flash_message_text, "flash_message_class": flash_message_class})
+    return templates.TemplateResponse(
+        "index.html",
+        {"request": request, "flash_message_text": flash_message_text, "flash_message_class": flash_message_class}
+    )
+@app.get("/favicon.ico", include_in_schema=False)
+async def favicon():
+    # TODO: Edit your favicon.ico in the static folder
+    return FileResponse(C.FAVICON_PATH)
 
-
-@app.get("/page/{page_name}", response_class=HTMLResponse)
-async def page(request: Request, page_name: str):
-    data = {
-        "page": page_name
-    }
-    return templates.TemplateResponse("page.html", {"request": request, "data": data})
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(
+        "main:app", 
+        host="localhost", 
+        port=8080,
+        reload=True,
+        log_level="debug",
+    )
