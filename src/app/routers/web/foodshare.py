412156c74e2698b6c0aa46d423f3ed821a698054
Liu JiaJun <jiajunliupersonal@gmail.com>
2023-11-27 16:49:33 +0800

    Add FoodSharing HTML Pages
    Pages includes: My Donation Listing, Add Listing, Edit Listing

--- src/app/routers/web/foodshare.py
+++ src/app/routers/web/foodshare.py
@@ -20,15 +20,33 @@ foodshare_router = APIRouter(
     tags= ['FoodSharingPlatform']
 )
 
-@foodshare_router.get("/add")
+@foodshare_router.get("/addMyListing")
 async def index(request: Request) -> RedirectResponse:
-    flash(
-        request=request,
-        message="This is a test flash message", 
-        category="test",
-    )
+    # flash(
+    #     request=request,
+    #     message="This is a test flash message", 
+    #     category="success",
+    # )
     return await render_template(
         name="foodshare/addDonation.html",
+        context={
+            "request": request,
+        },
+    )
+
+@foodshare_router.get("/myListings")
+async def myListing(request: Request) -> RedirectResponse:
+    return await render_template(
+        name="foodshare/listMyDonations.html",
+        context={
+            "request": request,
+        },
+    )
+
+@foodshare_router.get("/editMyListing")
+async def myListing(request: Request) -> RedirectResponse:
+    return await render_template(
+        name="foodshare/editDonationInfo.html",
         context={
             "request": request,
         },
