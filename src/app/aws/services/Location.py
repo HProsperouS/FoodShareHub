e5847997a16be9d0d7f6c239e5cbebe20e2edb2c
Liu JiaJun <jiajunliupersonal@gmail.com>
2024-01-23 09:13:26 +0800

    Update contants.py

--- src/app/aws/services/Location.py
+++ src/app/aws/services/Location.py
@@ -3,14 +3,10 @@ import boto3
 from utils import constants as C
 
 def autocomplete_address(query_text, max_results=10):
-    client = boto3.client('location', 
-                          aws_access_key_id=C.Location_ACCESS_KEY, 
-                          aws_secret_access_key=C.Location_SECRET_KEY,
-                          region_name=C.Location_REGION
-                          )
+    client = boto3.client('location')
     
     response = client.search_place_index_for_suggestions(
-        IndexName='FoodShareHub',
+        IndexName='FoodShareHubIndex',
         Text=query_text,
         FilterCountries=["SGP"],
         MaxResults=max_results
