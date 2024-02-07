2ae7c590832248f2de5ade77ff10f5a168d3a03f
Zhe-Yin <tayzheyin123@gmail.com>
2024-02-07 17:28:17 +0800

    forget init

--- src/app/db/__init__.py
+++ src/app/db/__init__.py
@@ -18,7 +18,8 @@ from .crud.donation_crud import(
    get_all_donations_by_userid,
    get_donation_by_id,
    update_donation,
-   softdelete_donation
+   softdelete_donation,
+   get_all_donations_exclude_userid
 )
 
 # Search Related Methods
