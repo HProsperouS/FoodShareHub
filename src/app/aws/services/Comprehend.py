5ea28eee2ab333f758ec71ece0c10a2f6004129a
Liu JiaJun <jiajunliupersonal@gmail.com>
2024-02-07 22:23:47 +0800

    Create Comprehend.py
    Use of Comprehend to detect toxic words used in the chat

---
+++ src/app/aws/services/Comprehend.py
@@ -0,0 +1,31 @@
+import boto3
+
+def detect_malicious_content(text):  
+
+    comprehend = boto3.client(service_name='comprehend', region_name="us-east-1")
+
+    toxic_response = comprehend.detect_toxic_content(
+        TextSegments=[
+            {
+                'Text': text
+            }
+        ], 
+        LanguageCode='en'
+    )
+
+    threshold = 0.7
+    toxicity_score = toxic_response.get('ResultList', [{}])[0].get('Toxicity', 0)
+    if toxicity_score > threshold:
+        return True
+    
+    # Labels Method (Not used)
+    # labels = toxic_response.get('ResultList', [{}])[0].get('Labels', [])
+    # for label in labels:
+    #     if label['Score'] > threshold:
+    #         # If any score is above the threshold, return a True, means the content is toxic
+    #         return True
+
+    return False
+    
+# text = "Can you come my room? I want to show you something."
+# detect_malicious_content(text)
